a1c5f3981bbc1927583f0595d445975a761960b4
ml1150258 <mark.lehr@rcc.edu>
2016-06-30 14:01:30 -0700

    Signed-off-by: ml1150258 <mark.lehr@rcc.edu>

---
+++ Lab/Savitch_9thEd_Chap3_Prob6_Fibonacci_Crud/main.cpp
@@ -0,0 +1,74 @@
+/* 
+ * File:   main.cpp
+ * Author: Dr. Mark E. Lehr
+ * Created on June 30, 2016, 12:30 PM
+ * Purpose:  Fibonacci and Crud
+ */
+
+//System Libraries
+#include <iostream>  //Input/Output Library
+#include <iomanip>   //Formatting Library
+using namespace std; //Namespace of the System Libraries
+
+//User Libraries
+
+//Global Constants
+
+//Function Prototypes
+
+//Execution Begins Here!
+int main(int argc, char** argv) {
+    //Declare Variables
+    float crudWt,curntWt;//Crud weight in lbs
+    int fi=1,fip1=1,fip2;//Fibonacci sequence
+    int days=0;//Number of Days
+    
+    //Input Data
+    cout<<"How many lbs of crud do you have?"<<endl;
+    cin>>crudWt;
+    
+    //Process the Data - Day 0
+    curntWt=crudWt*fi;
+    cout<<"  Fi Days Weight"<<endl;
+    cout<<setw(4)<<fi<<setw(5)<<days<<setw(7)<<curntWt<<endl;
+    
+    //Process the Data - Day 5
+    curntWt=crudWt*fip1;
+    days+=5;
+    cout<<setw(4)<<fi<<setw(5)<<days<<setw(7)<<curntWt<<endl;
+    
+    //Process the Data - Day 10
+    fip2=fip1;
+    fip1=fi;
+    fi=fip1+fip2;
+    curntWt=crudWt*fi;
+    days+=5;
+    cout<<setw(4)<<fi<<setw(5)<<days<<setw(7)<<curntWt<<endl;
+    
+    //Process the Data - Day 15
+    fip2=fip1;
+    fip1=fi;
+    fi=fip1+fip2;
+    curntWt=crudWt*fi;
+    days+=5;
+    cout<<setw(4)<<fi<<setw(5)<<days<<setw(7)<<curntWt<<endl;
+    
+    //Process the Data - Day 20
+    fip2=fip1;
+    fip1=fi;
+    fi=fip1+fip2;
+    curntWt=crudWt*fi;
+    days+=5;
+    cout<<setw(4)<<fi<<setw(5)<<days<<setw(7)<<curntWt<<endl;
+    
+    //Process the Data - Day 30
+    fip2=fip1;
+    fip1=fi;
+    fi=fip1+fip2;
+    curntWt=crudWt*fi;
+    days+=5;
+    cout<<setw(4)<<fi<<setw(5)<<days<<setw(7)<<curntWt<<endl;
+    
+    //Exit Stage Right!
+    return 0;
+}
